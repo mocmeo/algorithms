0beb7d4e1c6401c7caf42498e80d53687595d479
mocmeo <vdev.lamnn@gmail.com>
2019-06-29 10:22:45 +0700

    Remove duplicates

--- remove-duplicates.py
+++ remove-duplicates.py
@@ -1,8 +1,16 @@
+import sys
+
+
 def removeDuplicates(nums):
-    arr = {}
-    for x in nums:
-        arr[x] = 1
-    return list(arr.keys())
+    for i in range(1, len(nums)):
+        if (nums[i] == nums[i-1]):
+            nums[i-1] = sys.maxsize
+
+    nums.sort()
+    if (sys.maxsize in nums):
+        pos = nums.index(sys.maxsize)
+        nums = nums[0:pos]
+    return len(nums)
 
 
-print(removeDuplicates([1, 1, 2]))
+print(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
