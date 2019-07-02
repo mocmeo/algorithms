d4253dbd76565966980f1815deae5f8630eb49d7
mocmeo <vdev.lamnn@gmail.com>
2019-07-02 20:04:59 +0700

    Add longest palindrome

---
+++ longest-palindrome.py
@@ -0,0 +1,23 @@
+def longestPalindrome(s):
+    strDict = {}
+    for x in s:
+        if strDict.get(x) is None:
+            strDict[x] = 1
+        else:
+            strDict[x] += 1
+
+    result = 0
+    oddChar = False
+    for x in strDict.keys():
+        if strDict[x] % 2 == 0:
+            result += strDict[x]
+        else:
+            if not oddChar:
+                result += strDict[x]
+                oddChar = True
+            else:
+                result += strDict[x] - 1
+    return result
+
+
+print(longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
