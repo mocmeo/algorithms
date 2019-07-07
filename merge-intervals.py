1a00748ffcac8f2249baf1cffe9c125e5f5a9fd6
mocmeo <vdev.lamnn@gmail.com>
2019-07-07 20:19:46 +0700

    Add merge intervals solution 2

--- merge-intervals.py
+++ merge-intervals.py
@@ -13,15 +13,27 @@ def merge(intervals):
     while x < len(intervals) - 1:
         common = checkMatch(intervals[x], intervals[x+1])
         if common:
-            del intervals[x]
-            del intervals[x]
-            intervals.insert(x, common)
+            del intervals[x+1]
+            intervals[x] = common
         else:
             x += 1
     return intervals
 
 
+def merge2(intervals):
+    intervals.sort(key=lambda x: x[0])
+
+    merged = []
+    for interval in intervals:
+        if not merged or merged[-1][1] < interval[0]:
+            merged.append(interval)
+        else:
+            maxEnd = max(merged[-1][1], interval[1])
+            merged[-1][1] = max(merged[-1][1], interval[1])
+    return merged
+
+
 # print(merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
-print(merge([[1, 4], [4, 5]]))
-print(merge([[4, 5], [7, 8]]))
-print(merge([[1, 4], [5, 6]]))
+print(merge2([[1, 4], [4, 5]]))
+print(merge2([[4, 5], [7, 8]]))
+print(merge2([[1, 4], [5, 6]]))
